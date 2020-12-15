786d0e409cbeef3bf0e6811f6451e5ee6fd93e9f
ST <st@STs-MacBook-Pro.local>
2020-12-15 20:02:14 +0900

    投稿の編集、削除まで作成。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'top' => 'home#top'
   get 'index' => 'home#index'
-  get 'service' => 'home#service'
+  get 'posts/index' => 'posts#index'
   get 'signup' => 'home#signup'
+  get '/' => 'home#index'
+
+  get '/posts/new' => 'posts#new'
+  post '/posts/create' => 'posts#create'
+
+  get '/posts/:id/edit' => 'posts#edit'
+  post '/posts/:id/update' => 'posts#update'
+
+  post '/posts/:id/destroy' => 'posts#destroy'
 end
